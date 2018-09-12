f2d1c8ffca0ea26b5b812163a60b46892b89cd10
Kurzdor <pavloeksmidt@gmail.com>
2018-09-12 23:21:08 +0300

    :fire: :white_check_mark: :arrow_up: Update 1.9.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,6 @@ var pump = require('pump');
 var sourcemaps = require('gulp-sourcemaps');
 var scss = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-// var bro = require('gulp-bro');
-// var babelify = require('babelify');
-// var uglifyify = require('uglifyify');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
 var gIf = require('gulp-if');
@@ -24,14 +21,14 @@ var dir = {
     js: './tmp/build/static/js/',
     img: './tmp/build/static/images/',
     css: './tmp/build/static/css/',
-    fonts: './tmp/build/static/fonts/',
+    fonts: './tmp/build/static/fonts/'
   },
   dist: {
     html: 'dist/',
     js: 'dist/static/js/',
     img: 'dist/static/images/',
     css: 'dist/static/css/',
-    fonts: 'dist/static/fonts/',
+    fonts: 'dist/static/fonts/'
   },
   src: {
     html: 'src/**/*.html',
@@ -58,125 +55,152 @@ var browserSyncConfig = {
   host: 'localhost',
   port: 9000,
   logPrefix: 'overbuffed-gulp',
-  open: false,
+  logConnections: true,
   ghostMode: false
 };
 
-var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+var isDevelopment =
+  !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 var gulpBuildMode = isDevelopment ? 'build-dev' : 'build-prod';
 
-console.log(isDevelopment ? '[OVERBUFFED-GULP] Building dev...' : '[OVERBUFFED-GULP] Building production...');
+console.log(
+  isDevelopment
+    ? '[OVERBUFFED-GULP] Building dev...'
+    : '[OVERBUFFED-GULP] Building production...'
+);
 
-gulp.task('favicon', function () {
-  return gulp.src('src/favicon.ico', { allowEmpty: true })
+gulp.task('favicon', function() {
+  return gulp
+    .src('src/favicon.ico', { allowEmpty: true })
     .pipe(gulp.dest(isDevelopment ? dir.build.html : dir.dist.html));
 });
 
-gulp.task('html', function (cb) {
-  return pump([
-    gulp.src(dir.src.html),
-    gIf(isDevelopment, changed(dir.build.html)),
-    cachebust({
-      type: 'timestamp'
-    }),
-    gulp.dest(isDevelopment ? dir.build.html : dir.dist.html),
-  ], cb);
+gulp.task('html', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.html),
+      gIf(isDevelopment, changed(dir.build.html)),
+      cachebust({
+        type: 'timestamp'
+      }),
+      gulp.dest(isDevelopment ? dir.build.html : dir.dist.html)
+    ],
+    cb
+  );
 });
 
-gulp.task('css', function (cb) {
-  return pump([
-    gulp.src(dir.src.css),
-    prefix({
-      browsers: ['last 3 versions', 'Firefox ESR', 'Safari >= 6']
-    }),
-    gIf(isDevelopment, changed(dir.build.css)),
-    gulp.dest(isDevelopment ? dir.build.css : dir.dist.css),
-  ], cb);
+gulp.task('css', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.css, { allowEmpty: true }),
+      prefix({
+        browsers: ['last 3 versions', 'Firefox ESR', 'Safari >= 6']
+      }),
+      gIf(isDevelopment, changed(dir.build.css)),
+      gulp.dest(isDevelopment ? dir.build.css : dir.dist.css)
+    ],
+    cb
+  );
 });
 
-gulp.task('scss', function (cb) {
-  return pump([
-    gulp.src(dir.src.scss),
-    gIf(isDevelopment, changed(dir.build.css)),
-    gIf(isDevelopment, sourcemaps.init()),
-    scss({
-      outputStyle: 'expanded',
-      includePaths: ['./node_modules/']
-    }).on('error', scss.logError),
-    prefix({
-      browsers: ['last 3 versions', 'Firefox ESR', 'Safari >= 6']
-    }),
-    gIf(isDevelopment, sourcemaps.write('./')),
-    gulp.dest(isDevelopment ? dir.build.css : dir.dist.css),
-  ], cb);
+gulp.task('scss', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.scss),
+      gIf(isDevelopment, changed(dir.build.css)),
+      gIf(isDevelopment, sourcemaps.init()),
+      gIf(
+        isDevelopment,
+        scss({
+          outputStyle: 'expanded',
+          includePaths: ['./node_modules/']
+        }).on('error', scss.logError)
+      ),
+      gIf(
+        !isDevelopment,
+        scss({
+          outputStyle: 'compressed',
+          includePaths: ['./node_modules/']
+        }).on('error', scss.logError)
+      ),
+      prefix({
+        browsers: ['last 3 versions', 'Firefox ESR', 'Safari >= 6']
+      }),
+      gIf(isDevelopment, sourcemaps.write('./')),
+      gulp.dest(isDevelopment ? dir.build.css : dir.dist.css)
+    ],
+    cb
+  );
 });
 
-// gulp.task('js', function (cb) {
-//   return pump([
-//     gulp.src(dir.src.js, { allowEmpty: true }),
-//     gIf(isDevelopment, sourcemaps.init()),
-//     bro({
-//       entries: ['index.js'],
-//       paths: ['.', '/node_modules'],
-//       transform: [
-//         babelify.configure({ presets: ['env'] }),
-//         [
-//           'uglifyify', {
-//             global: true
-//           }
-//         ]
-//       ]
-//     }),
-//     rename({
-//       basename: 'script'
-//     }),
-//     gIf(isDevelopment, sourcemaps.write()),
-//     gulp.dest(isDevelopment ? dir.build.js : dir.dist.js),
-//   ], cb);
-// });
+gulp.task('js-no-webpack', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.js, { allowEmpty: true }),
+      gIf(isDevelopment, sourcemaps.init()),
+      rename({
+        basename: 'script'
+      }),
+      gIf(isDevelopment, sourcemaps.write()),
+      gulp.dest(isDevelopment ? dir.build.js : dir.dist.js)
+    ],
+    cb
+  );
+});
 
 gulp.task('webpack', function(cb) {
-  return pump([
-    gulp.src(dir.src.js, { allowEmpty: true }),
-    gIf(isDevelopment,
-      gulpWebpack(require('./webpack.config.dev.js'), webpack )
-    ),
-    gIf(!isDevelopment,
-      gulpWebpack( require('./webpack.config.prod.js'), webpack )
-    ),
-    gulp.dest(isDevelopment ? dir.build.js : dir.dist.js),
-  ], cb);
+  return pump(
+    [
+      gulp.src(dir.src.js, { allowEmpty: true }),
+      gIf(
+        isDevelopment,
+        gulpWebpack(require('./webpack.config.dev.js'), webpack)
+      ),
+      gIf(
+        !isDevelopment,
+        gulpWebpack(require('./webpack.config.prod.js'), webpack)
+      ),
+      gulp.dest(isDevelopment ? dir.build.js : dir.dist.js)
+    ],
+    cb
+  );
 });
 
-gulp.task('img', function (cb) {
-  return pump([
-    gulp.src(dir.src.img),
-    gIf(isDevelopment, changed(dir.build.img)),
-    gIf(!isDevelopment, imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.jpegtran({ progressive: true }),
-      imagemin.optipng({ optimizationLevel: 2 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ])),
-    gulp.dest(isDevelopment ? dir.build.img : dir.dist.img),
-  ], cb);
+gulp.task('img', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.img),
+      gIf(isDevelopment, changed(dir.build.img)),
+      gIf(
+        !isDevelopment,
+        imagemin([
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.jpegtran({ progressive: true }),
+          imagemin.optipng({ optimizationLevel: 2 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+          })
+        ])
+      ),
+      gulp.dest(isDevelopment ? dir.build.img : dir.dist.img)
+    ],
+    cb
+  );
 });
 
-gulp.task('fonts', function (cb) {
-  return pump([
-    gulp.src(dir.src.fonts, { allowEmpty: true }),
-    gIf(isDevelopment, changed(dir.build.fonts)),
-    gulp.dest(isDevelopment ? dir.build.fonts : dir.dist.fonts),
-  ], cb);
+gulp.task('fonts', function(cb) {
+  return pump(
+    [
+      gulp.src(dir.src.fonts, { allowEmpty: true }),
+      gIf(isDevelopment, changed(dir.build.fonts)),
+      gulp.dest(isDevelopment ? dir.build.fonts : dir.dist.fonts)
+    ],
+    cb
+  );
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch(dir.watch.scss, gulp.series('scss'));
   gulp.watch(dir.watch.html, gulp.series('html'));
   gulp.watch(dir.watch.js, gulp.series('webpack'));
@@ -184,13 +208,67 @@ gulp.task('watch', function () {
   gulp.watch(dir.watch.fonts, gulp.series('fonts'));
 });
 
-gulp.task('server', function () {
-  bsync.init(browserSyncConfig);
-  bsync.watch("src/**/*.*").on('change', bsync.reload);
+gulp.task('watch-alt', function() {
+  gulp.watch(dir.watch.scss, gulp.series('scss'));
+  gulp.watch(dir.watch.html, gulp.series('html'));
+  gulp.watch(dir.watch.js, gulp.series('js-no-webpack'));
+  gulp.watch(dir.watch.img, gulp.series('img'));
+  gulp.watch(dir.watch.fonts, gulp.series('fonts'));
 });
 
-gulp.task('build-dev', gulp.series('img', 'scss', 'css', 'webpack', 'fonts', 'html', 'favicon', gulp.parallel('watch', 'server')));
+gulp.task('server', function() {
+  bsync.init(browserSyncConfig);
+  bsync.watch('tmp/**/*.*').on('change', bsync.reload);
+});
 
-gulp.task('build-prod', gulp.series(gulp.parallel('img', 'scss', 'css', 'webpack', 'fonts', 'html', 'favicon')));
+gulp.task(
+  'build-dev',
+  gulp.series(
+    'img',
+    'scss',
+    'css',
+    'webpack',
+    'fonts',
+    'html',
+    'favicon',
+    gulp.parallel('watch', 'server')
+  )
+);
+
+gulp.task(
+  'build-prod',
+  gulp.series(
+    gulp.parallel('img', 'scss', 'css', 'webpack', 'fonts', 'html', 'favicon')
+  )
+);
+
+gulp.task(
+  'build-dev-no-webpack',
+  gulp.series(
+    'img',
+    'scss',
+    'css',
+    'js-no-webpack',
+    'fonts',
+    'html',
+    'favicon',
+    gulp.parallel('watch-alt', 'server')
+  )
+);
+
+gulp.task(
+  'build-prod-no-webpack',
+  gulp.series(
+    gulp.parallel(
+      'img',
+      'scss',
+      'css',
+      'js-no-webpack',
+      'fonts',
+      'html',
+      'favicon'
+    )
+  )
+);
 
 gulp.task('default', gulp.series(gulpBuildMode));
